808c81f5cfaa89f011a9ce4449dbdbaccc0e96a2
Helen Durrant <helen.durrant@transferwise.com>
2020-05-12 17:36:31 +0100

    fix: removing next-css now that next supports css by default

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,7 +1,6 @@
 const getBranch = require('git-branch');
 const rehypePrism = require('@mapbox/rehype-prism');
 const rehypeSlug = require('rehype-slug');
-const withCSS = require('@zeit/next-css');
 const withFonts = require('next-fonts');
 const withImages = require('next-images');
 const withMDX = require('@next/mdx')({
@@ -9,7 +8,7 @@ const withMDX = require('@next/mdx')({
     rehypePlugins: [rehypeSlug, rehypePrism],
   },
 });
-const withTM = require('next-transpile-modules');
+const withTM = require('next-transpile-modules')([]);
 
 const pageExtensions = ['js', 'mdx'];
 
@@ -23,24 +22,22 @@ module.exports = () =>
   withTM(
     withImages(
       withFonts(
-        withCSS(
-          withMDX({
-            transpileModules: ['@transferwise/dynamic-flows'],
-            pageExtensions,
-            assetPrefix,
-            env: {
-              ASSET_PREFIX: assetPrefix,
-            },
-            webpack: (config) => {
-              config.module.rules.push({
-                test: /\.code.js$/,
-                use: 'raw-loader',
-              });
+        withMDX({
+          transpileModules: ['@transferwise/dynamic-flows'],
+          pageExtensions,
+          assetPrefix,
+          env: {
+            ASSET_PREFIX: assetPrefix,
+          },
+          webpack: (config) => {
+            config.module.rules.push({
+              test: /\.code.js$/,
+              use: 'raw-loader',
+            });
 
-              return config;
-            },
-          }),
-        ),
+            return config;
+          },
+        }),
       ),
     ),
   );
