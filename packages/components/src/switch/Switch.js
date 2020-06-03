a473fa8aa3a0e19c70701d689117b8415a3992a7
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-06-03 11:19:35 +0200

    chore: switch setup

--- packages/components/src/switch/Switch.js
+++ packages/components/src/switch/Switch.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Types from 'prop-types';
 
 /**
@@ -9,11 +9,27 @@ import Types from 'prop-types';
  * @usage '<Switch/>'
  **/
 
-const Switch = (props) => {
-  return ();
+const Switch = ({ onClick }) => {
+  const [checked, setChecked] = useState(false);
+  const handleOnClick = () => {
+    setChecked(!checked);
+  };
+
+  useEffect(() => {
+    onClick(checked);
+  }, [checked]);
+
+  return (
+    <>
+      {console.log(checked)}
+      <input type="checkbox" checked={checked} onClick={handleOnClick} />
+    </>
+  );
 };
 
-Switch.propTypes = {};
+Switch.propTypes = {
+  onClick: Types.func.isRequired,
+};
 Switch.defaultProps = {};
 
 export default Switch;
--- packages/components/src/switch/Switch.story.js
+++ packages/components/src/switch/Switch.story.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Switch from './Switch';
 
-
 export default {
   component: Switch,
   title: 'Switch',
 };
 
 export const basic = () => {
-  return (
-    <Switch/>
-  );
+  return <Switch onClick={(checked) => alert(checked)} />;
 };
