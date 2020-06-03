2ad6efccd6f756dc2cb768fd79d641101235a96e
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-06-03 10:26:01 +0200

    feat: add switch component

---
+++ packages/components/src/switch/Switch.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Types from 'prop-types';
+
+/**
+ * Switch docs.
+ *
+ * @param {type} [propname=""] - propsDescription.
+ *
+ * @usage '<Switch/>'
+ **/
+
+const Switch = (props) => {
+  return ();
+};
+
+Switch.propTypes = {};
+Switch.defaultProps = {};
+
+export default Switch;
---
+++ packages/components/src/switch/Switch.spec.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Switch from './';
+import { render } from '@testing-library/react';
+
+describe('Switch', () => {
+    it('your test here', () => {});
+});
