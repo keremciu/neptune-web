f119a468e63ba8c97a55a6e1d6281eaadef3e763
Andrea Piras <16908937+andreapiras@users.noreply.github.com>
2020-06-04 13:08:54 +0200

    chore: update jest file

--- packages/components-scaffold/react/templates/jest.js
+++ packages/components-scaffold/react/templates/jest.js
@@ -1,6 +1,8 @@
 const templateJestFile = (name) => `import React from 'react';
 import ${name} from './';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import user from '@testing-library/user-event';
 
 describe('${name}', () => {
     it('your test here', () => {});
