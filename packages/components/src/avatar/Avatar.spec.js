29ce0732efee615d2d4231f004404e46f532808d
Richard Erdos <richard.erdos@transferwise.com>
2020-06-08 09:47:50 +0100

    feat: Add tests for Avatar

---
+++ packages/components/src/avatar/Avatar.spec.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Avatar from './';
+
+describe('Avatar', () => {
+  let props = {
+    size: Avatar.Size.LARGE,
+    children: '😎',
+  };
+  let wrapper;
+
+  beforeEach(() => (wrapper = shallow(<Avatar {...props} />)));
+
+  it('should render the given children', () => {
+    expect(wrapper.text()).toBe(props.children);
+  });
+
+  it('should have size modifier on the base element', () => {
+    expect(wrapper.hasClass('avatar--lg')).toBe(true);
+  });
+
+  it('should NOT have outline modifier on the base element', () => {
+    expect(wrapper.hasClass('avatar--outlined')).toBe(false);
+  });
+
+  describe('when we pass outlined prop', () => {
+    beforeEach(() => wrapper.setProps({ outlined: true }));
+
+    it('should have an outline modifier on the base element', () => {
+      expect(wrapper.hasClass('avatar--outlined')).toBe(true);
+    });
+  });
+});
